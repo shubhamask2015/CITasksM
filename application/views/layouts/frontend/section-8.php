724241762ce7d130d8c2e0d88ef24d3135fbcf9a
shubhamak9 <127405459+shubhamak9@users.noreply.github.com>
2024-04-29 11:21:22 +0530

    section 8 updated

--- application/views/layouts/frontend/section-8.php
+++ application/views/layouts/frontend/section-8.php
@@ -5,6 +5,41 @@
             <p class="p2">Our Clients Testimonial</p>
             <p class="p3">Lorem issum dolor sil amel, consectelue alipiscing elit. Ut elil tellus, luctus noc ullamcorper mattis, pulvinar dapibus Ico.</p>
         </div>
-        <div class="row2"></div>
+        <div class="row2">
+            <div class="col1">
+                <p class="p1"><i class="fas fa-quote-left"></i></p>
+                <p class="p2">Sed ut perspiciatis unde omnis iste natus error sit voluptater
+                    accusantium dolor emque laudantium, tocam rem apertamn.
+                    eaque ipsa quae ab illo inventore veritatis et quasi architecto
+                    beatae vitae dicta sunt explicabo.</p>
+                <div class="profile">
+                    <div class="circle"></div>
+                    <div class="name">
+                        <p class="p11">John De</p>
+                        <p class="p22">DIRECTOR</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col2">
+                <p class="p1"><i class="fas fa-quote-left"></i></p>
+                <p class="p2">Sed ut perspiciatis unde omnis iste natus error sit voluptater
+                    accusantium dolor emque laudantium, tocam rem apertamn.
+                    eaque ipsa quae ab illo inventore veritatis et quasi architecto
+                    beatae vitae dicta sunt explicabo.</p>
+                <div class="profile">
+                    <div class="circle"></div>
+                    <div class="name">
+                        <p class="p11">John De</p>
+                        <p class="p22">DIRECTOR</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row3">
+            <i class="fas fa-circle" style="color:#888;"></i>
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+        </div>
     </div>
 </section>
